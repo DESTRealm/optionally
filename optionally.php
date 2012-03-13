07ff4930ce59512680a6496a6afe0b8f8feb3472
Benjamin A. Shelton <bshelton@destrealm.org>
2012-03-13 02:47:54 -0600

    Includes have been updated to reflect additions/changes.

--- optionally.php
+++ optionally.php
@@ -3,4 +3,7 @@
 namespace org\destrealm\utilities\optionally;
 
 include dirname(__FILE__).DIRECTORY_SEPARATOR.'exceptions.php';
+include dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.
+    'Getopt.php';
+include dirname(__FILE__).DIRECTORY_SEPARATOR.'Options.class.php';
 include dirname(__FILE__).DIRECTORY_SEPARATOR.'Optionally.class.php';
