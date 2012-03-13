d826e2bddb7c43918695047a87db0e5dde62a58d
Benjamin A. Shelton <bshelton@destrealm.org>
2012-03-13 02:46:13 -0600

    Added new Options class as a container for processed options.

--- Options.class.php
+++ Options.class.php
@@ -5,14 +5,109 @@ namespace org\destrealm\utilities\optionally;
 class Options
 {
 
-    public function __construct ($options)
+    private $_options = array();
+
+    private $_args = array();
+
+    public function __construct ($options, $settings, $optionMap)
     {
+        $this->_options = $this->parseOptions($options[0], $settings, $optionMap);
+
+        // Handle boolean false options.
+        foreach ($optionMap as $option => $master) {
+            if (!array_key_exists($option, $this->_options) &&
+                $settings[$master]['boolean'] === true) {
+                foreach ($settings[$option]['aliases'] as $alias) {
+                    $this->_options[$alias] = false;
+                }
+                $this->_options[$master] = false;
+            }
+        }
+
+        // Add the arguments to our tracker.
+        $this->_args = $options[1];
 
     } // end constructor
 
     public function __get ($value)
     {
+        // Bail early if the option cache exists for this entry.
+        if (array_key_exists($value, $this->_options)) {
+            return $this->_options[$value];
+        }
 
+        if (array_key_exists($value, $this->_options)) {
+            return $this->_options[$value];
+        }
+
+        if (!property_exists($this, $value)) {
+            return null;
+        }
     } // end __get ()
+
+    public function args () { return $this->_args; }
+
+
+    /**
+     * Parses $options, compares the values contained within against
+     * $this->options, and returns an array containing the option names as keys
+     * and their checked values as values.
+     * @param array $options Options to parse.
+     * @param array $optionMap Map containing as keys a list of all known
+     * aliases pointing to values of their parent option.
+     * @return array Parsed values.
+     */
+    private function parseOptions ($options, $settings, $optionMap)
+    {
+        $values = array();
+
+        foreach ($options as $option) {
+
+            $opt = $option[0]; // Option name.
+            $val = $option[1]; // Option value. Might be empty.
+
+            // Filter long options.
+            $opt = str_replace('--', '', $opt);
+
+            // Preemptively set the option to our captured value.
+            $values[$opt] = $val;
+
+            $attributes = null;
+
+            // Find which option we're honoring; this is one of the master
+            // $this->options key-value store or something in the $optionMap
+            // which will lead us there.
+            if (array_key_exists($opt, $settings)) {
+                $attributes = $settings[ $opt ];
+            } else if (in_array($opt, $optionMap)) {
+                $attributes = $settings[ $optionMap[$opt] ];
+            } else {
+                continue;
+            }
+
+            // If the value is empty and we're expecting a value, and this value
+            // is *not* optional, throw an error.
+            if ($attributes['value'] && !$attributes['optionalValue'] &&
+                empty($val)) {
+                throw new OptionallyParserException(
+                    'Value is required for option.',
+                    $opt,
+                    OptionallyException::REQUIRES_ARGUMENT
+                );
+            }
+
+            // Boolean true. False is handled elsewhere.
+            if ($attributes['boolean'] === true && empty($val)) {
+                $values[$opt] = true;
+            }
+
+            // Assign all aliases to the same value.
+            foreach ($attributes['aliases'] as $alias) {
+                $values[$alias] = $values[$opt];
+            }
+        }
+
+        return $values;
+    } // end parseOptions ()
 
 } // end Options
