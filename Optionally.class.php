8647ace1692f46bf32796432577398daef0ce88e
Benjamin A. Shelton <bshelton@destrealm.org>
2012-03-14 00:12:53 -0600

    Added comments and license text.

--- Optionally.class.php
+++ Optionally.class.php
@@ -8,6 +8,26 @@ namespace org\destrealm\utilities\optionally;
  * related to quirks in both PHP and its author, it does adhere to many of the
  * same principles first introduced in popular usage by Optimist for handling
  * command line arguments.
+ *
+ * Copyright (c) 2012 Benjamin A. Shelton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
 class Optionally
 {
@@ -24,6 +44,10 @@ class Optionally
      */
     private $argv = array();
 
+    /**
+     * Local PEAR Getopt reference.
+     * @var Console_Getopt
+     */
     private $getopt = null;
 
     /**
@@ -40,6 +64,12 @@ class Optionally
      */
     private $options = array();
 
+    /**
+     * Option settings template.
+     * This private var serves as the template for all options that are created
+     * using Optionally::option() and dictates (hopefully) reasonable defaults.
+     * @var array
+     */
     private $optionTemplate = array(
         'aliases' => array(),   /* Aliases for a given option. */
         'description' => null,  /* Option description. Shown by help(). */
