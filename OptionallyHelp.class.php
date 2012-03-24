db434370e1e562ecee3cc19fcd89495fec91d692
Benjamin A. Shelton <bshelton@destrealm.org>
2012-03-23 23:23:40 -0600

    Preliminary add of help-text formatting support.

---
+++ OptionallyHelp.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class OptionallyHelp
+{
+
+    public function __construct ($options)
+    {
+
+    } // end constructor
+
+    /**
+     * Parses the description $desc.
+     * @param  string $desc Description.
+     * @return string       Parsed description.
+     */
+    private static function parseDescription ($desc)
+    {
+
+    } // end parseDescription ()
+
+    /**
+     * Parses the example $example.
+     * @param  string $example Example.
+     * @return string          Parsed example.
+     */
+    private static function parseExample ($example)
+    {
+
+    } // end parseExample ()
+
+} // end OptionallyHelp
--- optionally.php
+++ optionally.php
@@ -34,3 +34,4 @@ include dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.
     'Getopt.php';
 include dirname(__FILE__).DIRECTORY_SEPARATOR.'Options.class.php';
 include dirname(__FILE__).DIRECTORY_SEPARATOR.'Optionally.class.php';
+include dirname(__FILE__).DIRECTORY_SEPARATOR.'OptionallyHelp.class.php';
