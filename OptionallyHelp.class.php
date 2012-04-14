5994ac4875cafa4437e4e0380b3949b8e12300ce
Benjamin A. Shelton <bshelton@destrealm.org>
2012-04-14 15:05:50 -0600

    Adding updates to partially-working help object!

--- OptionallyHelp.class.php
+++ OptionallyHelp.class.php
@@ -5,14 +5,36 @@ namespace org\destrealm\utilities\optionally;
 class OptionallyHelp
 {
 
+    /**
+     * Buffer between options and their associated help text, in spaces.
+     * @var integer
+     */
     private $buffer = 2;
 
+    /**
+     * Option cutoff before placing it on a separate line.
+     * @var integer
+     */
     private $cutoff = 20;
 
+    /**
+     * Maximum output columns.
+     * @var integer
+     */
     private $columns = 80;
 
     private $help = array();
 
+    /**
+     * Indentation for aliases as displayed following the primary option.
+     * @var integer
+     */
+    private $indentAliases = 4;
+
+    /**
+     * Maximum length of all options.
+     * @var integer
+     */
     private $maxLength = 0;
 
     public function __construct ()
@@ -26,10 +48,14 @@ class OptionallyHelp
             $aliases = array($aliases);
         }
 
+        $option = $this->toOption($option);
+
         $this->maxLength = max(strlen($option), $this->maxLength);
 
-        foreach ($aliases as $alias) {
-            $this->maxLength = max(strlen($option), $this->maxLength);
+        foreach ($aliases as &$alias) {
+            $alias = $this->toOption($alias);
+            $this->maxLength = max(strlen($alias)+$this->indentAliases,
+                $this->maxLength);
         }
 
         $this->help[$option] = array(
@@ -54,30 +80,80 @@ class OptionallyHelp
 
         foreach ($keys as $key) {
 
-            /*$alias = 0;
-            $line = '';
+            $aliases = $this->help[$key]['aliases'];
+
+            // Indented help text, one element per line.
+            $help = explode("\n",
+                String::indent(
+                    String::wrap(
+                        String::normalize($this->help[$key]['description'])
+                    ),
+                    $this->maxLength + $this->buffer
+                )
+            );
 
             if (strlen($key) <= $this->cutoff) {
-                $line .= $key . str_repeat(' ', $this->buffer);
+                $line = array_shift($help);
+                if (($optLine = String::replaceIndent(
+                        str_repeat(' ', $this->maxLength + $this->buffer),
+                        $key,
+                        $line
+                    )) !== false) {
+                    $line = $optLine;
+                }
+                $buf .= $line."\n";
             } else {
                 $buf .= $key."\n";
             }
 
-            while ($pos < strlen($this->help[$key]['description'])) {
-                $line .= substr(
-                    $this->help[$key]['description'],
-                    $pos,
-                    $this->columns - strlen($line) - $this->buffer
-                );
+            $max = count($aliases);
+            for ($i = 0; $i < $max; $i++) {
 
-                $buf .= $line."\n";
-            }*/
+                $alias = array_shift($aliases);
+
+                if (empty($help)) {
+                    break;
+                }
+
+                if (strlen($alias) <= $this->cutoff) {
+                    $line = array_shift($help);
+                    $line = String::replaceIndent(
+                        str_repeat(' ', $this->maxLength + $this->buffer),
+                        str_repeat(' ', $this->indentAliases).$alias,
+                        $line
+                    );
+                    $buf .= $line."\n";
+                } else {
+                    $buf .= $alias."\n";
+                }
+
+            }
+
+            if (!empty($aliases)) {
+                $buf .= implode("\n", $aliases);
+            } else if (!empty($help)) {
+                $buf .= implode("\n", $help);
+            }
+
+            $buf .= "\n";
+
         }
+
+        return $buf;
     } // end help ()
 
     public function show ()
     {
 
     } // end show ()
+
+    private function toOption ($option)
+    {
+        if (strlen($option) === 1) {
+            return '-'.$option;
+        }
+
+        return '--'.$option;
+    } // end toOption ()
 
 } // end OptionallyHelp
