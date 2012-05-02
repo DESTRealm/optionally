34c236b209fb5481b2f6a595c7105564ec6608bf
Benjamin A. Shelton <bshelton@destrealm.org>
2012-05-02 03:27:50 -0600

    Added HelpUsageTestCase.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,6 @@
 
 require './tests/BaseTestCase.php';
 require './tests/Common/HelpTestCase.php';
+require './tests/Common/HelpUsageTestCase.php';
 require './tests/Common/OptionallyTestCase.php';
 require './tests/Common/StringTestCase.php';
