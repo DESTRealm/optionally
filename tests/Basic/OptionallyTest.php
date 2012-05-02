5ffa526aeb7a8b476a377b5c5100e2237af9be67
Benjamin A. Shelton <bshelton@destrealm.org>
2012-05-02 01:57:38 -0600

    Isolated unit test code in tests/Common and individual use cases in tests/Basic, tests/NoAutoload, and tests/Phar.

---
+++ tests/Basic/OptionallyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DESTRealm\Optionally\Tests\Basic;
+
+use DESTRealm\Optionally\Tests\Common\OptionallyTestCase;
+
+class OptionallyTest extends OptionallyTestCase
+{
+    public function setUp ()
+    {
+        require 'autoload.php';
+    } // end setUp ()
+} // end OptionallyTest ()
---
+++ tests/Basic/StringTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DESTRealm\Optionally\Tests\Basic;
+
+use DESTRealm\Optionally\Tests\Common\StringTestCase;
+
+class StringTest extends StringTestCase
+{
+    public function setUp ()
+    {
+        require 'autoload.php';
+    } // end setUp ()
+} // end StringTest ()
