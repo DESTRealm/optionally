9368c9243d732cd30930b2b687b952832cafe341
Benjamin A. Shelton <bshelton@destrealm.org>
2012-05-02 01:41:21 -0600

    Moved BasicTest -> BasicTestCase.

---
+++ tests/BasicTestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DESTRealm\Optionally\Tests;
+
+class BasicTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp ()
+    {
+        require 'autoload.php';
+    } // end setUp ()
+} // end BasicTest
