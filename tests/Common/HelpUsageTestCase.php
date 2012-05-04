fe93139f2f500db13c663d1bf34b9e7625f49267
Benjamin A. Shelton <bshelton@destrealm.org>
2012-05-04 03:36:27 -0600

    Added another assertion to check long options with hyphens.

--- tests/Common/HelpUsageTestCase.php
+++ tests/Common/HelpUsageTestCase.php
@@ -12,8 +12,10 @@ class HelpUsageTestCase extends BaseTestCase
     {
         $options = Optionally::options(array('./script.php', '--debug'))
             ->option('debug')
+                ->boolean()
                 ->describe('This option will attempt to enable debugging.')
-            ->argv();
+            ->argv()
+            ;
 
         $this->assertEquals(
 'Usage: ./script.php [options]
@@ -23,4 +25,59 @@ class HelpUsageTestCase extends BaseTestCase
             $options->help()
         );
     } // end testSimpleUsage ()
+
+    public function testMultipleOptions ()
+    {
+        $options = Optionally::options(array(
+            './script.php',
+            '--debug',
+            '-c',
+            '--with-stdout',
+            '--without-stderr',
+        ))
+            ->option('debug')
+                ->alias('d')
+                ->boolean()
+                ->describe('This option enables debugging mode for the script.')
+            ->option('config')
+                ->alias('c')
+                ->value('')
+                ->describe('This option instructs the script which configuration
+                    file to use if %@file is provided.')
+            ->option('with-stdout')
+                ->boolean()
+                ->describe('Enable STDOUT output.')
+            ->option('without-stdout')
+                ->boolean()
+                ->describe('Disable STDOUT output.')
+            ->option('with-stderr')
+                ->boolean()
+                ->describe('Enable STDERR output.')
+            ->option('without-stderr')
+                ->boolean()
+                ->describe('Disable STDERR output.')
+            ->argv()
+            ;
+            print $options->help();
+
+        $this->assertEquals(
+'Usage: ./script.php [options]
+
+--config[=][file]  This option instructs the script which configuration file to
+    -c [file]      use if [file] is provided.
+
+--debug            This option enables debugging mode for the script.
+    -d
+
+--with-stderr      Enable STDERR output.
+
+--with-stdout      Enable STDOUT output.
+
+--without-stderr   Disable STDERR output.
+
+--without-stdout   Disable STDOUT output.
+',
+            $options->help()
+        );
+    } // end testMultipleOptions ()
 } // end HelpUsageTestCase
