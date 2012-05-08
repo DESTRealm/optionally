e54b91b96fcaba6e717e7c6ce28ff45b67bc588c
Benjamin A. Shelton <bshelton@destrealm.org>
2012-05-08 03:19:36 -0600

    Brining tests up to parity with the documentation.

--- tests/Common/ReadmeTestCase.php
+++ tests/Common/ReadmeTestCase.php
@@ -22,8 +22,7 @@ class ReadmeTestCase extends BaseTestCase
 {
 
     /**
-     * This test should match up with the sources under the "Basic Usage"
-     * heading.
+     * Basic Usage
      */
     public function testBasicUsage ()
     {
@@ -42,5 +41,234 @@ class ReadmeTestCase extends BaseTestCase
         $this->assertEquals('output.txt', $args[1]);
 
     } // end testBaseUsage ()
+
+    /**
+     * Optionally and Options
+     */
+    public function testOptionallyAndOptions ()
+    {
+
+        $_SERVER['argv'] = array('script.php', '--test=1', '-v', '--debug', 'file.txt', 'output.txt');
+        $options = Optionally::options()
+          ->option('test')
+            ->value()
+          ->option('v')
+            ->boolean()
+          ->option('debug')
+            ->boolean()
+          ->argv()
+          ;
+        $this->assertEquals('1', $options->test);
+        $this->assertTrue($options->v);
+        $this->assertTrue($options->debug);
+
+        $_SERVER['argv'] = array('script.php', '--test=1', '-v', 'file.txt', 'output.txt');
+        $options = Optionally::options()
+          ->option('test')
+            ->value()
+          ->option('v')
+            ->boolean()
+          ->option('debug')
+            ->boolean()
+          ->argv()
+          ;
+        $this->assertEquals('1', $options->test);
+        $this->assertTrue($options->v);
+        $this->assertFalse($options->debug);
+
+        $_SERVER['argv'] = array('script.php', '-v', 'file.txt', 'output.txt');
+        $options = Optionally::options()
+          ->option('test')
+            ->value()
+          ->option('v')
+            ->boolean()
+          ->option('debug')
+            ->boolean()
+          ->argv()
+          ;
+        $this->assertNull($options->test);
+
+    } // end testOptionallyAndOptions ()
+
+    /**
+     * Options Object
+     */
+    public function testOptionsObject ()
+    {
+
+        $_SERVER['argv'] = array('script.php', '--without-foo');
+        $options = Optionally::options()
+          ->option('with-bar')
+            ->boolean()
+          ->option('without-foo')
+            ->boolean()
+          ->argv()
+          ;
+        $this->assertTrue($options->withoutFoo);
+        $this->assertTrue($options->without_foo);
+        $this->assertFalse($options->withBar);
+        $this->assertFalse($options->with_bar);
+
+    } // end testOptionsObject ()
+
+    /**
+     * Advanced Options: Aliases!
+     */
+    public function testAdvancedOptionsAliases ()
+    {
+
+        $_SERVER['argv'] = array('script.php', '-v');
+        $options = Optionally::options()
+          ->option('v')
+            ->boolean()
+            ->alias('verbose')
+          ->argv()
+          ;
+        $this->assertTrue($options->v);
+        $this->assertTrue($options->verbose);
+
+        $_SERVER['argv'] = array('script.php', '--debug');
+        $options = Optionally::options()
+          ->option('debug')
+            ->alias('d')
+            ->boolean()
+          ->option('v')
+            ->boolean()
+            ->alias('verbose')
+          ->argv()
+          ;
+        $this->assertTrue($options->debug);
+        $this->assertTrue($options->d);
+        $this->assertFalse($options->v);
+        $this->assertFalse($options->verbose);
+    } // end testAdvancedOptionsAliases ()
+
+    /**
+     * Advanced Options: Optional Values!
+     */
+    public function testAdvancedOptionsOptionalValues ()
+    {
+
+        $_SERVER['argv'] = array('script.php', '-v', '--number', '--count=5');
+        $options = Optionally::options()
+          ->option('v')
+            ->alias('verbose')
+            ->boolean()
+          ->option('number')
+            ->alias('n')
+            ->value(0)
+          ->option('count')
+            ->value(0)
+          ->option('max')
+            ->value(0)
+          ->argv()
+          ;
+        $this->assertTrue($options->v);
+        $this->assertTrue($options->verbose);
+        $this->assertEquals(0, $options->number);
+        $this->assertEquals(0, $options->n);
+        $this->assertEquals(5, $options->count);
+        $this->assertEquals(0, $options->max);
+
+        $_SERVER['argv'] = array('script.php', '--count');
+        $options = Optionally::options()
+          ->option('count')
+            ->alias('c')
+            ->value('')
+          ->argv()
+          ;
+        $this->assertEquals('', $options->count);
+
+        $_SERVER['argv'] = array('script.php');
+        $options = Optionally::options()
+          ->option('count')
+            ->alias('c')
+            ->value('')
+          ->argv()
+          ;
+        $this->assertEquals('', $options->count);
+
+        $_SERVER['argv'] = array('script.php', '--count=15');
+        $options = Optionally::options()
+          ->option('count')
+            ->alias('c')
+            ->value('')
+          ->argv()
+          ;
+        $this->assertEquals(15, $options->count);
+    } // end testAdvancedOptionsOptionalValues ()
+
+    /**
+     * Really Advanced Options: Mostly Optional Values with Difference Defaults!
+     */
+    public function testAdvancedOptionsMovwdd ()
+    {
+        $_SERVER['argv'] = array('script.php');
+        $options = Optionally::options()
+          ->option('count')
+            ->value()
+              ->optional()
+              ->defaults(0)
+              ->defaultsIfMissing(false)
+          ->argv()
+          ;
+        $this->assertFalse($options->count);
+
+        $_SERVER['argv'] = array('script.php', '--count');
+        $options = Optionally::options()
+          ->option('count')
+            ->value()
+              ->optional()
+              ->defaults(0)
+              ->defaultsIfMissing(false)
+          ->argv()
+          ;
+        $this->assertEquals(0, $options->count);
+
+        $_SERVER['argv'] = array('script.php', '--count', '15');
+        $options = Optionally::options()
+          ->option('count')
+            ->value()
+              ->optional()
+              ->defaults(0)
+              ->defaultsIfMissing(false)
+          ->argv()
+          ;
+        $this->assertEquals(15, $options->count);
+    } // end testAdvancedOptionsMovwdd ()
+
+    /**
+     * Really Advanced Options: Countable Options as Array Options!
+     */
+    public function testAdvancedOptionsCountableArrays ()
+    {
+        $_SERVER['argv'] = array('script.php', '-v', '-v', 'file.txt');
+        $options = Optionally::options()
+          ->option('verbose')
+            ->alias('v')
+            ->isCountable() // countable() is an alias to this.
+          ->argv()
+          ;
+        $this->assertEquals('2', $options->verbose);
+        $this->assertEquals('2', $options->v);
+
+        $_SERVER['argv'] = array('script.php', '-v', '-v', '-v', '-v');
+        $options = Optionally::options()
+          ->option('verbose')
+            ->alias('v')
+            ->isCountable() // countable() is an alias to this.
+          ->argv()
+          ;
+        $this->assertEquals('4', $options->verbose);
+        $this->assertEquals('4', $options->v);
+
+        $_SERVER['argv'] = array('script.php', '--filter', 'bw', '--filter', 'mosaic');
+        $options = Optionally::options()
+          ->option('filter')
+            ->isArray()
+          ->argv()
+          ;
+        $this->assertEquals(array('bw', 'mosaic'), $options->filter);
+    } // end testAdvancedOptionsCountableArrays ()
 
 } // end ReadmeTestCase
