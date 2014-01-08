4d0fa5c745c51f365a98bd5c1150aa8909c65c21
Benjamin A. Shelton <bshelton@destrealm.org>
2014-01-07 22:35:29 -0700

    Updated bootstrap to use composer's autoload.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,4 @@
 <?php
 
-require './tests/BaseTestCase.php';
-require './tests/Common/HelpTestCase.php';
-require './tests/Common/HelpUsageTestCase.php';
-require './tests/Common/OptionallyTestCase.php';
-require './tests/Common/StringTestCase.php';
-require './tests/Common/ReadmeTestCase.php';
+$loader = include __DIR__.'/../vendor/autoload.php';
+$loader->add('PluggableIdeas', __DIR__.'/../src/');
